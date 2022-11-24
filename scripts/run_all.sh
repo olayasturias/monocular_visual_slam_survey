c050397712649bc3a715cc17ab08e79794df8afd
olayasturias <olaya_93@hotmail.es>
2022-11-24 14:46:50 +0100

    orb_slam3_mono working on all datasets

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
+datasetRoot=$HOME/Datasets
+echo "########################################################################"
+echo "########################### ORB-SLAM3 ##################################"
+echo "########################################################################"
 
-./run_orb3_mono.sh Aqualoc /home/olaya/Datasets/Aqualoc/Archaeological_site_sequences
+echo "######################## Aqualoc  dataset ##############################"
+
+datasetName=Aqualoc/Archaeological_site_sequences
+tracks='1'
+./run_orb3_mono.sh $datasetName $datasetRoot "${tracks[@]}"    
+
+echo "############################# MIMIR ####################################"
+datasetName=MIMIR
+tracks='SeaFloor/track0'
+./run_orb3_mono.sh $datasetName $datasetRoot "${tracks[@]}"  
+
+echo "############################# EuRoC ####################################"
+datasetName=EuRoC
+tracks='MH_04_difficult'
+./run_orb3_mono.sh $datasetName $datasetRoot "${tracks[@]}"  
+
+echo "########################### TUM-RGBD ###################################"
+datasetName=TUM
+tracks='rgbd_dataset_freiburg1_360'
+./run_orb3_mono.sh $datasetName $datasetRoot "${tracks[@]}"  
--- scripts/run_orb3_mono.sh
+++ scripts/run_orb3_mono.sh
@@ -2,11 +2,56 @@
 
 datasetName=$1
 datasetRoot=$2
+tracks=$3
+orb_vocab=../algorithms/ORB_SLAM3/Vocabulary/ORBvoc.txt
 
-tracks='1'
 for track in $tracks
 do 
-echo "Launching $track with Monocular sensor"
-../algorithms/ORB_SLAM3/Examples/Monocular/mono_aqualoc ../algorithms/ORB_SLAM3/Vocabulary/ORBvoc.txt ../algorithms/ORB_SLAM3/Examples/Monocular/Aqualoc.yaml "$datasetRoot"/archaeo_sequence_$track\_raw_data $track
-mv KeyFrameTrajectory.txt ../results/$datasetName\/kf_archaeo_sequence_$track\_raw_data.txt
+echo "Launching ORB_SLAM for track $track from $datasetName dataset with Monocular sensor"
+if [[ $datasetName == *"Aqualoc"* ]]; then
+  configfile=../algorithms/ORB_SLAM3/Examples/Monocular/Aqualoc.yaml
+  resultfile=KeyFrameTrajectory.txt
+  ../algorithms/ORB_SLAM3/Examples/Monocular/mono_aqualoc $orb_vocab $configfile "$datasetRoot"/"$datasetName"/archaeo_sequence_$track\_raw_data $track
+
+fi
+if [[ $datasetName == *"MIMIR"* ]]; then
+  configfile=../algorithms/ORB_SLAM3/Examples/Monocular/MIMIR.yaml
+  resultfile=kf_out.txt
+  ../algorithms/ORB_SLAM3/Examples/Monocular/mono_mimir $orb_vocab $configfile "$datasetRoot"/MIMIR/"$track"/auv0/rgb/cam0/data ../algorithms/ORB_SLAM3/Examples/Monocular/MIMIR_TimeStamps/"$track"/timestamps.txt out
+fi
+
+if [[ $datasetName == *"EuRoC"* ]]; then
+  configfile=../algorithms/ORB_SLAM3/Examples/Monocular/EuRoC.yaml
+  resultfile=kf_out.txt
+  ../algorithms/ORB_SLAM3/Examples/Monocular/mono_euroc $orb_vocab $configfile "$datasetRoot"/EuRoC/"$track" ../algorithms/ORB_SLAM3/Examples/Monocular/EuRoC_TimeStamps/"$track".txt out
+fi
+
+if [[ $datasetName == *"KITTI"* ]]; then
+  if [ $track == *"00"* ]||[ $track == *"01"* ]||[ $track == *"02"* ]; then
+    configfile=../algorithms/ORB_SLAM3/Examples/Monocular/KITTI00-02.yaml
+  elif [[ $track == *"03"* ]]; then
+    configfile=../algorithms/ORB_SLAM3/Examples/Monocular/KITTI03.yaml
+  else
+    configfile=../algorithms/ORB_SLAM3/Examples/Monocular/KITTI04-12.yaml
+  fi
+  resultfile=KeyFrameTrajectory.txt
+  ../algorithms/ORB_SLAM3/Examples/Monocular/mono_kitti $orb_vocab $configfile "$datasetRoot"/KITTI/"$track"
+fi
+
+if [[ $datasetName == *"TUM"* ]]; then
+  if [[ $track == *"g1"* ]]; then
+    configfile=../algorithms/ORB_SLAM3/Examples/Monocular/TUM1.yaml
+  elif [[ $track == *"g2"* ]]; then
+    configfile=../algorithms/ORB_SLAM3/Examples/Monocular/TUM2.yaml
+  elif [[ $track == *"g3"* ]]; then
+    configfile=../algorithms/ORB_SLAM3/Examples/Monocular/TUM3.yaml
+  fi
+  resultfile=KeyFrameTrajectory.txt
+  ../algorithms/ORB_SLAM3/Examples/Monocular/mono_tum $orb_vocab $configfile "$datasetRoot"/TUM/"$track" 
+fi
+
+mkdir --parents ../results/ORB_SLAM3/"$datasetName"/"$track"
+mv $resultfile ../results/ORB_SLAM3/"$datasetName"/"$track"/kf_out.txt
+
+
 done
