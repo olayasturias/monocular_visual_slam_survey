35e17142a03176dd2e649fbb6e7576d807cf9e1e
olayasturias <olaya_93@hotmail.es>
2022-11-28 16:39:27 +0100

    script for dfvo

--- scripts/generate_dfvo_configs.sh
+++ scripts/generate_dfvo_configs.sh
@@ -6,11 +6,37 @@ model_zoo_dir=$4
 
 for track in $tracks
 do
-  if [[ $datasetName == *"EuRoC"* ]]; then
-    mkdir --parents ../configs/DFVO/"$datasetName"/"$track"
-    export sequence_name=$track
-    export result_directory="../results/DFVO/"$datasetName"/"$sequence_name
-    export dataset_directory=$datasetRoot"/"$datasetName
+  export sequence_name=$track
+  export result_directory="../results/DFVO/"$datasetName"/"$sequence_name
+  export dataset_directory=$datasetRoot"/"$datasetName
+  mkdir --parents ../configs/DFVO/"$datasetName"
+  mkdir --parents ../results/DFVO/"$datasetName"/"$sequence_name"
+
+
+  if [[ $datasetName == *"MIMIR"* ]]; then
+
+    export sequence_name="$(dirname $track)" 
+    export track="$(basename $track)" 
+
+    mkdir --parents ../configs/DFVO/"$datasetName"/"$sequence_name"
+    mkdir --parents "../results/DFVO/"$datasetName"/"$sequence_name
+        
+    ( echo "cat <<EOF >../configs/DFVO/"$datasetName"/"$sequence_name"/"$track".yml";
+      cat ../algorithms/DF-VO/options/examples/"$datasetName"/default_configuration.yml;
+      echo "EOF";
+    ) >temp.yml
+    . temp.yml
+    rm -f temp.yml
+  else
+    if [[ $datasetName == *"TUM"* ]]; then
+      if [[ $track == *"g1"* ]]; then
+        export tum_room=tum-1
+      elif [[ $track == *"g2"* ]]; then
+        export tum_room=tum-2
+      elif [[ $track == *"g3"* ]]; then
+        export tum_room=tum-3
+      fi
+    fi
     ( echo "cat <<EOF >../configs/DFVO/"$datasetName"/"$sequence_name".yml";
       cat ../algorithms/DF-VO/options/examples/"$datasetName"/default_configuration.yml;
       echo "EOF";
@@ -18,55 +44,5 @@ do
     . temp.yml
     rm -f temp.yml
   fi
-  if [[ $datasetName == *"MIMIR"* ]]; then
-    export seqname="$(dirname $track)" 
-    export trackname="$(basename $track)" 
-    mkdir --parents ../configs/DFVO/"$datasetName"/"$seqname"
-    mkdir --parents "../results/DFVO/"$datasetName"/"$seqname
-    export result_directory="../results/DFVO/"$datasetName"/"$track
-    export dataset_directory=$datasetRoot"/"$datasetName
-    export sequence_name="$track"
-    ( echo "cat <<EOF >../configs/DFVO/"$datasetName"/"$seqname"/"$trackname".yml";
-      cat ../algorithms/DF-VO/options/examples/"$datasetName"/default_configuration.yml;
-      echo "EOF";
-    ) >temp.yml
-    . temp.yml
-    rm -f temp.yml
-  fi
-  if [[ $datasetName == *"TUM"* ]]; then
-    if [[ $track == *"g1"* ]]; then
-      export tum_room=tum-1
-    elif [[ $track == *"g2"* ]]; then
-      export tum_room=tum-2
-    elif [[ $track == *"g3"* ]]; then
-      export tum_room=tum-3
-    fi
-    export seqname="$(dirname $track)" 
-    export trackname="$(basename $track)" 
-    mkdir --parents ../configs/DFVO/"$datasetName"/"$seqname"
-    mkdir --parents "../results/DFVO/"$datasetName"/"$seqname
-    export result_directory="../results/DFVO/"$datasetName"/"$track
-    export dataset_directory=$datasetRoot"/"$datasetName
-    export sequence_name="$track"
-    ( echo "cat <<EOF >../configs/DFVO/"$datasetName"/"$seqname"/"$trackname".yml";
-      cat ../algorithms/DF-VO/options/examples/"$datasetName"/default_configuration.yml;
-      echo "EOF";
-    ) >temp.yml
-    . temp.yml
-    rm -f temp.yml
-  fi
-    if [[ $datasetName == *"Aqualoc"* ]]; then
-    export sequence_name=$track
-    mkdir --parents ../configs/DFVO/"$datasetName"/"$seqname"
-    mkdir --parents "../results/DFVO/"$datasetName"/"$seqname
-    mkdir --parents ../configs/DFVO/"$datasetName"/"$track"
-    export result_directory="../results/DFVO/"$datasetName"/"$track
-    export dataset_directory=$datasetRoot"/"$datasetName
-    ( echo "cat <<EOF >../configs/DFVO/"$datasetName"/"$seqname"/"$track".yml";
-      cat ../algorithms/DF-VO/options/examples/"$datasetName"/default_configuration.yml;
-      echo "EOF";
-    ) >temp.yml
-    . temp.yml
-    rm -f temp.yml
-  fi
+
 done
--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -6,25 +6,25 @@ echo "# Algorithm 1: DF-VO #####################################################
 echo "####################################################################################"
 
 echo "#### 1.1 dataset TUM-RGBD ########################################################"
-# datasetName=TUM
-# tracks='rgbd_dataset_freiburg1_360'
-# ./generate_dfvo_configs.sh $datasetName $datasetRoot "${tracks[@]}" ../algorithms/DF-VO/model_zoo
-# export PYTHONPATH="${PYTHONPATH}:$PWD/../algorithms/DF-VO"
-# ./run_dfvo.sh $datasetName $datasetRoot "${tracks[@]}" 
+datasetName=TUM
+tracks='rgbd_dataset_freiburg1_360'
+./generate_dfvo_configs.sh $datasetName $datasetRoot "${tracks[@]}" ../algorithms/DF-VO/model_zoo
+export PYTHONPATH="${PYTHONPATH}:$PWD/../algorithms/DF-VO"
+./run_dfvo.sh $datasetName $datasetRoot "${tracks[@]}" 
 
 echo "#### 1.2 dataset MIMIR ###############################################################"
-# datasetName=MIMIR
-# tracks='SeaFloor/track0'
-# ./generate_dfvo_configs.sh $datasetName $datasetRoot "${tracks[@]}" ../algorithms/DF-VO/model_zoo
-# export PYTHONPATH="${PYTHONPATH}:$PWD/../algorithms/DF-VO"
-# ./run_dfvo.sh $datasetName $datasetRoot "${tracks[@]}" 
+datasetName=MIMIR
+tracks='SeaFloor/track0'
+./generate_dfvo_configs.sh $datasetName $datasetRoot "${tracks[@]}" ../algorithms/DF-VO/model_zoo
+export PYTHONPATH="${PYTHONPATH}:$PWD/../algorithms/DF-VO"
+./run_dfvo.sh $datasetName $datasetRoot "${tracks[@]}" 
 
 echo "#### 1.3 dataset EuRoC ###############################################################"
-# datasetName=EuRoC
-# tracks='MH_04_difficult'
-# ./generate_dfvo_configs.sh $datasetName $datasetRoot "${tracks[@]}" ../algorithms/DF-VO/model_zoo
-# export PYTHONPATH="${PYTHONPATH}:$PWD/../algorithms/DF-VO"
-# ./run_dfvo.sh $datasetName $datasetRoot "${tracks[@]}"
+datasetName=EuRoC
+tracks='MH_04_difficult'
+./generate_dfvo_configs.sh $datasetName $datasetRoot "${tracks[@]}" ../algorithms/DF-VO/model_zoo
+export PYTHONPATH="${PYTHONPATH}:$PWD/../algorithms/DF-VO"
+./run_dfvo.sh $datasetName $datasetRoot "${tracks[@]}"
 
 echo "#### 1.4 dataset Aqualoc #############################################################"
 datasetName=Aqualoc/Archaeological_site_sequences
@@ -33,12 +33,9 @@ export PYTHONPATH="${PYTHONPATH}:$PWD/../algorithms/DF-VO"
 ./generate_dfvo_configs.sh $datasetName $datasetRoot "${tracks[@]}" ../algorithms/DF-VO/model_zoo
 ./run_dfvo.sh $datasetName $datasetRoot "${tracks[@]}" 
 
-# datasetName=Aqualoc/Archaeological_site_sequences
-# tracks='1'
-# ./run_orb3_mono.sh $datasetName $datasetRoot "${tracks[@]}"   
 
 # echo "########################################################################"
-# echo "########################### ORB-SLAM3 ##################################"
+# echo "# Algorithm 2: ORB-SLAM3 ###############################################"
 # echo "########################################################################"
 
 # echo "######################## Aqualoc  dataset ##############################"
